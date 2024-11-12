785c908bf0b046fa82b038f5561fe4034f49b519
Михаил Грасин <583michail@mail.ru>
2024-11-12 21:17:12 +0300

    Menu -> Backend [Scrollable Pizzas]

--- WPF-PizzaDelivery/MainWindow.xaml.cs
+++ WPF-PizzaDelivery/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -22,14 +23,117 @@ namespace WPF_PizzaDelivery
     /// </summary>
     public partial class MainWindow : Window
     {
-        const string PHONE_NUMBER_ALPHABET = "1234567890+";
-
         public MainWindow()
         {
             InitializeComponent();
+
+            var table = FindName("Menu_GRD_Pizzas") as Grid;
+//            var grd = table.FindName("Menu_GRD_Pizza") as Grid;
+//            grd.Visibility = Visibility.Hidden;
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                var row = new RowDefinition { Height = new GridLength(300) };
+
+                table.RowDefinitions.Add(row);
+            }
+
+            for (int i = 0; i < table.RowDefinitions.Count; i++)
+            {
+                for (int j = 0; j < table.ColumnDefinitions.Count; j++)
+                {
+                    //var pizza = XamlReader.Parse(XamlWriter.Save(grd)) as Grid;
+
+                    var pizza = new Grid
+                    {
+                        HorizontalAlignment = HorizontalAlignment.Center,
+                        VerticalAlignment = VerticalAlignment.Center,
+                        Width = 200,
+                        Height = 250
+                    };
+
+                    {
+                        pizza.RowDefinitions.Add(new RowDefinition { Height = new GridLength(200) });
+                        pizza.RowDefinitions.Add(new RowDefinition { });
+
+                        var btn = new Button
+                        {
+                            Width = 200,
+                            Height = 200,
+                            Style = FindResource("MaterialDesignFlatLightButton") as Style
+                        };
+
+                        pizza.Children.Add(btn);
+
+                        {
+                            var grid = new Grid
+                            {
+                                HorizontalAlignment = HorizontalAlignment.Center,
+                                VerticalAlignment = VerticalAlignment.Center,
+                                Width = 200,
+                                Height = 200
+                            };
+
+                            btn.Content = grid;
+
+                            {
+                                grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(150) });
+                                grid.RowDefinitions.Add(new RowDefinition { });
+                                grid.RowDefinitions.Add(new RowDefinition { });
+
+                                var img = new Image
+                                {
+                                    Source = new BitmapImage(new Uri("HamNCheese.png", UriKind.Relative)),
+                                    HorizontalAlignment = HorizontalAlignment.Center,
+                                    VerticalAlignment = VerticalAlignment.Center,
+                                    Width = 145,
+                                    Height = 145,
+                                    Stretch = Stretch.UniformToFill
+                                };
+
+                                grid.Children.Add(img);
+
+                                var lbl = new Label
+                                {
+                                    Content = "Ветчина и сыр",
+                                    HorizontalAlignment = HorizontalAlignment.Center,
+                                    VerticalAlignment = VerticalAlignment.Center,
+                                    FontWeight = FontWeights.SemiBold
+                                };
+
+                                Grid.SetRow(lbl, 1);
+                                grid.Children.Add(lbl);
+
+                                lbl = new Label
+                                {
+                                    Content = "от 389 р.",
+                                    HorizontalAlignment = HorizontalAlignment.Center,
+                                    VerticalAlignment = VerticalAlignment.Center
+                                };
+
+                                Grid.SetRow(lbl, 2);
+                                grid.Children.Add(lbl);
+                            }
+                        }
+
+                        btn = new Button
+                        {
+                            Content = "Выбрать"
+                        };
+
+                        Grid.SetRow(btn, 1);
+                        pizza.Children.Add(btn);
+                    }
+
+                    Grid.SetRow(pizza, i);
+                    Grid.SetColumn(pizza, j);
+                    table.Children.Add(pizza);
+                }
+            }
         }
 
-        private void Login_TB_PhoneNumber_onTextChanged(object sender, TextChangedEventArgs e)
+            private void Login_TB_PhoneNumber_onTextChanged(object sender, TextChangedEventArgs e)
         {
             var tb = sender as TextBox;
 
