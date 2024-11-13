56f1625e6e8a23fc5c2694698283fd256ed8a8d8
Михаил Грасин <583michail@mail.ru>
2024-11-13 13:22:37 +0300

    Cart -> Scrollable Orders (WIP)

--- WPF-PizzaDelivery/MainWindow.xaml.cs
+++ WPF-PizzaDelivery/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Diagnostics;
 using System.Text.RegularExpressions;
+using MD = MaterialDesignThemes.Wpf;
 
 namespace WPF_PizzaDelivery
 {
@@ -27,9 +28,19 @@ namespace WPF_PizzaDelivery
         {
             InitializeComponent();
 
+            load();
+        }
+
+        void load()
+        {
+            loadMenuPage();
+        }
+
+        void loadMenuPage()
+        {
             var table = FindName("Menu_GRD_Pizzas") as Grid;
-//            var grd = table.FindName("Menu_GRD_Pizza") as Grid;
-//            grd.Visibility = Visibility.Hidden;
+            var grd = table.FindName("Menu_GRD_Pizza") as Grid;
+            grd.Visibility = Visibility.Hidden;
 
 
             for (int i = 0; i < 10; i++)
@@ -133,7 +144,119 @@ namespace WPF_PizzaDelivery
             }
         }
 
-            private void Login_TB_PhoneNumber_onTextChanged(object sender, TextChangedEventArgs e)
+        void loadCartPage()
+        {
+            var stack = FindName("Cart_SP_Orders") as StackPanel;
+
+            for (int i = 0; i < 10; i++)
+            {
+                var card = new MD.Card
+                {
+                    Width = 500,
+                    Height = 150,
+                    Padding = new Thickness(10)
+                };
+
+                stack.Children.Add(card);
+
+                {
+                    var grid = new Grid
+                    {
+                        HorizontalAlignment = HorizontalAlignment.Center,
+                        VerticalAlignment = VerticalAlignment.Center,
+                        Width = 200,
+                        Height = 200
+                    };
+
+                    card.Content = grid;
+
+                    {
+                        grid.RowDefinitions.Add(new RowDefinition { MinHeight = 100 });
+                        grid.RowDefinitions.Add(new RowDefinition { });
+
+                        grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(100) });
+                        grid.ColumnDefinitions.Add(new ColumnDefinition { });
+                        grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(50) });
+
+                        var bd = new Border { Padding = new Thickness(7.5) };
+                        {
+                            var img = new Image
+                            {
+                                Source = new BitmapImage(new Uri("HamNCheese.png", UriKind.Relative)),
+                                HorizontalAlignment = HorizontalAlignment.Center,
+                                VerticalAlignment = VerticalAlignment.Center
+                            };
+
+                            bd.Child = img;
+                        }
+
+                        grid.Children.Add(bd);
+
+                        var grid1 = new Grid
+                        {
+                            HorizontalAlignment = HorizontalAlignment.Center,
+                            VerticalAlignment = VerticalAlignment.Center,
+                            Width = 200,
+                            Height = 200
+                        };
+
+                        Grid.SetColumn(grid1, 1);
+                        grid.Children.Add(grid1);
+
+                        {
+                            grid1.RowDefinitions.Add(new RowDefinition { Height = new GridLength(40) });
+                            grid1.RowDefinitions.Add(new RowDefinition { });
+
+                            var lbl = new Label
+                            {
+                                Content = "Ветчина и сыр",
+                                FontWeight = FontWeights.Bold,
+                                FontSize = 20
+                            };
+
+                            grid1.Children.Add(lbl);
+
+                            lbl = new Label
+                            {
+                                Content = "30 см, традиционное тесто",
+                                FontWeight = FontWeights.Bold,
+                                FontSize = 13,
+                                Foreground = new SolidColorBrush(Colors.Gray)
+                            };
+
+                            Grid.SetRow(lbl, 1);
+                            grid1.Children.Add(lbl);
+                        }
+
+                        var btn = new Button
+                        {
+                            Style = FindResource("MaterialDesignFlatButton") as Style,
+                            HorizontalAlignment = HorizontalAlignment.Center,
+                            VerticalAlignment = VerticalAlignment.Center,
+                            Padding = new Thickness(0),
+                            Margin = new Thickness(0, 0, 0, 45),
+                            Width = 32,
+                            Height = 32,
+                            Foreground = new SolidColorBrush(Colors.Black)
+                        };
+
+                        Grid.SetColumn(btn, 2);
+                        grid.Children.Add(btn);
+
+                        {
+                            var icon = new MD.PackIcon
+                            {
+                                Kind = MD.PackIconKind.Close
+                            };
+
+                            btn.Content = icon;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void Login_TB_PhoneNumber_onTextChanged(object sender, TextChangedEventArgs e)
         {
             var tb = sender as TextBox;
 
